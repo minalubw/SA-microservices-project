3dc3a5548e46cca8e33a0ef26a41d6b86c3fffd2
Henok M mehreteab <michaelhenok.github.io>
2023-02-06 16:23:42 -0600

    eurekaFirstcommit

---
+++ eureka-service/src/test/java/com/example/saeuruka/SaEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.saeuruka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
