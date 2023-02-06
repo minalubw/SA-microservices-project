7435e1e77cc734092fd5039f56932187f40a19c6
Alazar A. Alemayehu <alazar.anbesi@miu.edu>
2023-02-06 17:12:32 -0600

    Config commit

--- TeacherService/src/main/java/saproject/teacherservice/domain/School.java
+++ TeacherService/src/main/java/saproject/teacherservice/domain/School.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 @Setter
 @ToString
 @RequiredArgsConstructor
-@NoArgsConstructor
+
 @AllArgsConstructor
 @Entity
 @Document(collection = "schools")
--- TeacherService/src/main/java/saproject/teacherservice/domain/SchoolDto.java
+++ TeacherService/src/main/java/saproject/teacherservice/domain/SchoolDto.java
@@ -8,7 +8,7 @@ import java.io.Serializable;
 /**
  * A DTO for the {@link School} entity
  */
-@EqualsAndHashCode(callSuper = true)
-@Data
+
+
 public record SchoolDto(String name, String address, Contact contact) implements Serializable {
 }
